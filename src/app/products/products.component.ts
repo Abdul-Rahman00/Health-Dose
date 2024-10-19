927a4bb1c4cf7c7106f1be810726148e107738f6
Abdul-Rahman00 <abdelrahman.ma657@gmail.com>
2024-10-19 15:48:05 +0200

    adding new files

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
